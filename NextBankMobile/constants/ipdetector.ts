e6fea6e94e0c2edc2bf40fef75dc819ae340bcc0
AKONGA MANUEL <akongawilfrid@gmail.com>
2026-04-24 12:58:40 +0100

    feat: new premium logo and android security bypass

--- NextBankMobile/constants/ipdetector.ts
+++ NextBankMobile/constants/ipdetector.ts
@@ -1,16 +1,6 @@
 import Constants from 'expo-constants';
 
 export const getHostIp = () => {
-  // Expo automatically knows the IP of the computer running the bundler!
-  // It's stored in Constants.expoConfig.hostUri
-  const hostUri = Constants.expoConfig?.hostUri;
-  
-  if (hostUri) {
-    // hostUri usually looks like: "10.97.5.72:8082"
-    // We just want the IP part before the colon
-    return hostUri.split(':')[0];
-  }
-  
-  // Fallback to Production VPS IP
+  // Always use Production VPS IP to ensure connectivity
   return '51.254.134.173';
 };
